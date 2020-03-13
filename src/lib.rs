5f408d288499fb9ba7e9f30ed9c2ab733b044b64
Alexander Polakov <a.polakov@iconic.vc>
2020-03-13 19:05:32 +0300

    Add delay

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ pub use runtime::Runtime;
 pub use task_executor::TaskExecutor;
 
 pub mod timer {
+    use futures_01::future::Future as Future01;
     use futures_01::stream::Stream as Stream01;
     use futures_01::Poll as Poll01;
     use futures_util::compat::Compat;
@@ -14,11 +15,19 @@ pub mod timer {
     use std::marker::PhantomData;
     use std::pin::Pin;
     use std::task::{Context, Poll};
+    use tokio_02::time::Delay as Delay2;
+    use tokio_02::time::Instant;
     use tokio_02::time::Interval as Interval2;
 
     struct PhantomError<S, E> {
         phantom: PhantomData<fn() -> E>,
-        stream: S,
+        inner: S,
+    }
+
+    impl<S, E> PhantomError<S, E> {
+        fn get_mut(&mut self) -> &mut S {
+            &mut self.inner
+        }
     }
 
     impl<S, E> futures_util::stream::Stream for PhantomError<S, E>
@@ -32,11 +41,27 @@ pub mod timer {
             cx: &mut Context,
         ) -> std::task::Poll<Option<Self::Item>> {
             let inner = Pin::into_inner(self);
-            let stream = Pin::new(&mut inner.stream);
+            let stream = Pin::new(&mut inner.inner);
             match stream.poll_next(cx) {
                 Poll::Pending => Poll::Pending,
                 Poll::Ready(Some(val)) => Poll::Ready(Some(Ok(val))),
                 Poll::Ready(None) => Poll::Ready(None),
+            }
+        }
+    }
+
+    impl<F, E> std::future::Future for PhantomError<F, E>
+    where
+        F: std::future::Future + Unpin,
+    {
+        type Output = Result<F::Output, E>;
+
+        fn poll(self: Pin<&mut Self>, cx: &mut Context) -> std::task::Poll<Self::Output> {
+            let inner = Pin::into_inner(self);
+            let stream = Pin::new(&mut inner.inner);
+            match stream.poll(cx) {
+                Poll::Pending => Poll::Pending,
+                Poll::Ready(val) => Poll::Ready(Ok(val)),
             }
         }
     }
@@ -46,7 +71,7 @@ pub mod timer {
     impl Interval {
         pub fn new_interval(duration: std::time::Duration) -> Self {
             Interval(Compat::new(PhantomError {
-                stream: tokio_02::time::interval(duration),
+                inner: tokio_02::time::interval(duration),
                 phantom: PhantomData,
             }))
         }
@@ -58,6 +83,34 @@ pub mod timer {
 
         fn poll(&mut self) -> Poll01<Option<Self::Item>, Self::Error> {
             self.0.poll()
+        }
+    }
+
+    pub struct Delay {
+        inner: Compat<PhantomError<Delay2, tokio_timer_02::Error>>,
+    }
+
+    impl Delay {
+        pub fn new(instant: Instant) -> Delay {
+            Delay {
+                inner: Compat::new(PhantomError {
+                    inner: tokio_02::time::delay_until(instant),
+                    phantom: PhantomData,
+                }),
+            }
+        }
+
+        pub fn reset(&mut self, deadline: Instant) {
+            self.inner.get_mut().get_mut().reset(deadline)
+        }
+    }
+
+    impl Future01 for Delay {
+        type Item = ();
+        type Error = tokio_timer_02::Error;
+
+        fn poll(&mut self) -> Poll01<Self::Item, Self::Error> {
+            self.inner.poll()
         }
     }
 }
